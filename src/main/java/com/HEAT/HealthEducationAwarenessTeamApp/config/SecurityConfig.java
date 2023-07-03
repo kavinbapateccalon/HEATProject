e0535c67cd8db189a85df2ec2afc6dff319079d4
Imanol Herrero <i.herrero@wustl.edu>
2023-07-02 21:18:10 -0400

    removed v1 used from tutorial

--- src/main/java/com/HEAT/HealthEducationAwarenessTeamApp/config/SecurityConfig.java
+++ src/main/java/com/HEAT/HealthEducationAwarenessTeamApp/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/HEAT/HealthEducationAwarenessTeamApp/welcome/WelcomeController.java
+++ src/main/java/com/HEAT/HealthEducationAwarenessTeamApp/welcome/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/welcome")
+@RequestMapping("/api/welcome")
 public class WelcomeController {
     @GetMapping
     public ResponseEntity<String> sayHello() {
